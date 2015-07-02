e23fba1b99d792ac5a8edc38341d9a779067c1a5
costia <p.costia@gmail.com>
2015-07-02 21:14:39 +0300

    initial commit

---
+++ layout.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name           Rss
+// @description    Rss
+// @include        https://*/*
+// @include        http://*/*
+// @version        0.01
+// @namespace https://github.com/costia/WrongKeyboardLayoutFix
+// @updateURL https://github.com/costia/WrongKeyboardLayoutFix/raw/master/layout.user.js
+// @downloadURL https://github.com/costia/WrongKeyboardLayoutFix/raw/master/layout.user.js
+// @grant        none
+// ==/UserScript==
+
+var oldOnKey=document.onkeydown;
+var enKeys="qwertyuiopasdfghjkl;zxcvbnm,.";
+var otherKeys="/'קראטוןםפשדגכעיחלךףזסבהנמצתץ";
+
+document.onkeydown = function(evt) {
+    focused=document.activeElement;
+    if (oldOnKey){
+        oldOnKey(evt);
+    }
+    if (evt.keyCode == 119) {
+        focusedElement=document.activeElement;
+        if (focusedElement.type=="text"){
+            inStr=focusedElement.value;
+            for (i=0;i<enKeys.length;i++){
+                inStr=inStr.replace(new RegExp("["+enKeys[i]+"]", "g"),otherKeys[i]);
+            }
+            focusedElement.value=inStr;
+        }
+        
+    }
+};
+
