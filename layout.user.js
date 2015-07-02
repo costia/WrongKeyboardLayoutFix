465c5d0c4a15e1b87fd4678c9b5d9678830e4187
costia <p.costia@gmail.com>
2015-07-02 21:23:53 +0300

    both ways
    both ways

--- layout.user.js
+++ layout.user.js
@@ -3,7 +3,7 @@
 // @description    WrongKeyboardLayoutFix
 // @include        https://*/*
 // @include        http://*/*
-// @version        0.01
+// @version        0.02
 // @namespace https://github.com/costia/WrongKeyboardLayoutFix
 // @updateURL https://github.com/costia/WrongKeyboardLayoutFix/raw/master/layout.user.js
 // @downloadURL https://github.com/costia/WrongKeyboardLayoutFix/raw/master/layout.user.js
@@ -23,12 +23,23 @@ document.onkeydown = function(evt) {
         focusedElement=document.activeElement;
         if (focusedElement.type=="text"){
             inStr=focusedElement.value;
+            countEn=0;
+            countOther=0;
             for (i=0;i<enKeys.length;i++){
-                inStr=inStr.replace(new RegExp("["+enKeys[i]+"]", "g"),otherKeys[i]);
+                tempRef=inStr.match(new RegExp("["+enKeys[i]+"]", "g"));
+                if (tempRef) countEn+=tempRef.length;
+                tempRef=inStr.match(new RegExp("["+otherKeys[i]+"]", "g"));
+                if (tempRef) countOther+=tempRef.length;
+            }
+            for (i=0;i<enKeys.length;i++){
+                if (countEn>=countOther){
+                    inStr=inStr.replace(new RegExp("["+enKeys[i]+"]", "g"),otherKeys[i]);
+                }else{
+                    inStr=inStr.replace(new RegExp("["+otherKeys[i]+"]", "g"),enKeys[i]);
+                }
             }
             focusedElement.value=inStr;
         }
         
     }
 };
-
